1cbbb199a89815776bc7a6c9fbefc87a74cafabc
ved2805 <vedrecharla@gmail.com>
2022-11-09 22:31:49 +0530

    404 check

--- src/components/Birthday/Birthday.js
+++ src/components/Birthday/Birthday.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Birthday = () => {
   return (
     <div class="alert alert-primary" role="alert">
-  Wish You A Very Very <a href="/hbday.html" class="alert-link " ><u>Happy Birthday</u></a> Nanna !
+  Wishing You A Very Very <a href="/hbday.html" class="alert-link " ><u>Happy Birthday</u></a> Nanna !
 </div>
   )
 }
