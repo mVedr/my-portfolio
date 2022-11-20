aededffc15469c060bd3035cff16c97e46ae2f77
ved2805 <vedrecharla@gmail.com>
2022-11-20 17:20:18 +0530

    removed ' from header

--- src/components/Birthday/Birthday.js
+++ src/components/Birthday/Birthday.js
@@ -4,7 +4,7 @@ const Birthday = () => {
   return (
     <>
       <div class="alert alert-success" role="alert">
- <a href="https://vedcreation.com/weather/index" class="alert-link " ><u>Click Here </u>To Check Today\'s Weather</a> Nanna !
+ <a href="https://vedcreation.com/windex" class="alert-link " ><u>Click Here </u>To Check Today\'s Weather</a> Nanna !
  <span> </span><span class="badge bg-primary">New Feature</span></div><div class="alert alert-primary" role="alert">
   Wishing You A Very Very <a href="https://vedcreation.com/bday" class="alert-link " ><u>Happy Birthday</u></a> Nanna !
   <span> </span><span class="badge bg-secondary">Archived</span></div>
