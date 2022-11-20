7eaaa82fae54a1825ac54f19bcd18a69007bcf4e
ved2805 <vedrecharla@gmail.com>
2022-11-20 16:58:31 +0530

    Adding Weather Api

--- src/components/Birthday/Birthday.js
+++ src/components/Birthday/Birthday.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Birthday = () => {
   return (
-    <div class="alert alert-primary" role="alert">
+    <>
+      <div class="alert alert-success" role="alert">
+ <a href="https://vedcreation.com/weather" class="alert-link " ><u>Click Here </u>To Check Today\'s Weather</a> Nanna !
+ <span> </span><span class="badge bg-primary">New Feature</span></div><div class="alert alert-primary" role="alert">
   Wishing You A Very Very <a href="https://vedcreation.com/bday" class="alert-link " ><u>Happy Birthday</u></a> Nanna !
-</div>
+  <span> </span><span class="badge bg-secondary">Archived</span></div>
+    </>
+    
   )
 }
 export default Birthday
